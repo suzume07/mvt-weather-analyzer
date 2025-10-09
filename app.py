5098c56f064f66980df8a0bb3fc566c0b2ba0872
suzume07 <137918849+suzume07@users.noreply.github.com>
2025-10-09 23:59:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,8 @@ else:
     else:
         st.info("Nhập tên thành phố và API key, sau đó nhấn **Lấy dữ liệu**.")
         st.stop()
-
+# Đổi tên timestamp_days
+df_display = df.rename(columns={"timestamp_days": "Thời gian (ngày)"})
 st.subheader(" 1. Dữ liệu đầu vào")
 st.dataframe(df.head())
 
