d192f415fb72504ce8a5e50e01ea4c4d1013a7c0
suzume07 <137918849+suzume07@users.noreply.github.com>
2025-10-11 16:30:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -90,9 +90,9 @@ elif option == "Lấy dữ liệu trực tiếp từ API":
         st.session_state.df_api = None
     
     if st.sidebar.button("Lấy dữ liệu"):
-        df_api = get_weather_data(city, api_key)
-    if df_api is not None:
-        st.session_state.df_api = df_api
+        df_new = get_weather_data(city, api_key)
+    if df_new is not None:
+        st.session_state.df_api = df_new
         st.session_state.api_key = api_key
         st.success(f" Đã tải thành công dữ liệu thời tiết của **{city}**!")
     else:
