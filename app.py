2f311aa0c5edd5afaa7bec3035ef27649c143742
suzume07 <137918849+suzume07@users.noreply.github.com>
2025-10-11 17:05:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ elif option == "Tải file CSV":
         st.stop()
 
 elif option == "Lấy dữ liệu trực tiếp từ API":
-    city = st.sidebar.text_input("Nhập tên thành phố:", city)
+    city = st.sidebar.text_input("Nhập tên thành phố:", "Hanoi")
     api_key = st.sidebar.text_input("Nhập API key OpenWeatherMap:", type="password")
 
     if st.sidebar.button("Lấy dữ liệu"):
